692c1adc61ff9ada00ae04ef3d44bc364c8861f0
Andy Lawrence <andy.lawrence@gmail.com>
2013-03-03 08:39:57 -0500

    test for printing a sequence of numbers

--- spec/lib/fizzbuzz_spec.rb
+++ spec/lib/fizzbuzz_spec.rb
@@ -11,4 +11,6 @@ describe 'FizzBuzz' do
     specify { buzzer.print(9).should == 'Fizz' }
     specify { buzzer.print(10).should == 'Buzz' }
     specify { buzzer.print(15).should == 'FizzBuzz' }
+
+    specify { buzzer.print_out(1..15).should == ['1','2', 'Fizz', '4', 'Buzz', '5', 'Fizz', '7', '8', 'Fizz', 'Buzz', '11', 'Fizz', '13', '14', 'FizzBuzz'] }
 end
