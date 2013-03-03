76c5c1d23e714e7ae94be9bc9c740aea852fc557
Andy Lawrence <andy.lawrence@gmail.com>
2013-03-03 06:44:47 -0500

    prints 'Fizz' for 6

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,7 @@
 class FizzBuzz
     def print(num)
         result = num.to_s
-        result = 'Fizz' if num == 3
+        result = 'Fizz' if num % 3 == 0
         result = 'Buzz' if num == 5
         return result
     end
